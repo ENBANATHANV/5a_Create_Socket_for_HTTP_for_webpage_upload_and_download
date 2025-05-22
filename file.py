3cefb21b7468f29da8d90de6b076f59e0fe5249e
ENBANATHANV <vijayenba34@gamil.com>
2025-05-22 17:59:15 +0530

    Success

--- file.py
+++ file.py
@@ -1,23 +1,4 @@
-# 5a_Create_Socket_for_HTTP_for_webpage_upload_and_download
-## AIM :
-To write a PYTHON program for socket for HTTP for web page upload and download
-## Algorithm
-
-1.Start the program.
-<BR>
-2.Get the frame size from the user
-<BR>
-3.To create the frame based on the user request.
-<BR>
-4.To send frames to server from the client side.
-<BR>
-5.If your frames reach the server it will send ACK signal to client otherwise it will send NACK signal to client.
-<BR>
-6.Stop the program
-<BR>
-## Program 
-### file 
-```import socket
+import socket
 import base64
 
 def send_request(host, port, request, headers=None):
@@ -62,9 +43,3 @@ if __name__ == "__main__":
     # Download file
     download_file(host, port, 'example.txt', headers)
     print("File downloaded successfully.")
-
-```
-## OUTPUT
-![alt text](444747234-628a6188-2d18-45a1-a905-fa92bd78a186.png)
-## Result
-Thus the socket for HTTP for web page upload and download created and Executed
